ac1b91147874512cc97ce9a2fe8c2f633804fd9b
Jef Spaleta <jef@sensu.io>
2019-04-02 14:13:22 -0800

    add travisCI specific build script

--- ruby-runtime/travis-build-ruby-plugin-assets.sh
+++ ruby-runtime/travis-build-ruby-plugin-assets.sh
@@ -1,45 +1,44 @@
 #!/bin/bash
-##
-# General asset build script
-##
 
+##
+# TravisCI specific asset build script.
+#   Uses several TravisCI specific environment variables 
+##
 [[ -z "$GITHUB_TOKEN" ]] && { echo "GITHUB_TOKEN is empty" ; exit 1; }
 [[ -z "$1" ]] && { echo "Parameter 1, GEM_NAME is empty" ; exit 1; }
-[[ -z "$2" ]] && { echo "Parameter 2, GIT_OWNER_REPO is empty" ; exit 1; }
-[[ -z "$3" ]] && { echo "Parameter 3, GIT_REF is empty" ; exit 1; }
+[[ -z "$TRAVIS_REPO_SLUG" ]] && { echo "TRAVIS_REPO_SLUG is empty" ; exit 1; }
+[[ -z "$TRAVIS_COMMIT" ]] && { echo "TRAVIS_COMMIT is empty" ; exit 1; }
 
 GEM_NAME=$1
-GIT_OWNER_REPO=$2
-GIT_REF=$3
-GITHUB_RELEASE_TAG=$4
-TAG=$GITHUB_RELEASE_TAG
-[[ -z "$TAG" ]] && { echo "GITHUB_RELEASE_TAG is empty" ; TAG="0.0.1"; }
-echo $GEM_NAME $GIT_OWNER_REPO $TAG $GIT_REF
+TAG=$TRAVIS_TAG
+[[ -z "$TAG" ]] && { echo "TRAVIS_TAG is empty" ; TAG="0.0.1"; }
+echo $GEM_NAME $TRAVIS_REPO_SLUG $TAG $TRAVIS_COMMIT
 
 mkdir dist
-GIT_REPO="https://github.com/${GIT_OWNER_REPO}.git"
+GIT_REPO="https://github.com/${TRAVIS_REPO_SLUG}.git"
+GIT_REF=${TRAVIS_COMMIT}
 
 
 if [ -d dist ]; then
   # Build Debian asset
-  docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-debian -f bonsai/Dockerfile.debian .
+  docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-debian -f Dockerfile.debian .
   docker cp $(docker create --rm ruby-plugin-debian:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/${GEM_NAME}_${TAG}_debian_linux_amd64.tar.gz
 
   # Build Alpine asset
-  docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-alpine:latest -f bonsai/Dockerfile.alpine .
+  docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-alpine:latest -f Dockerfile.alpine .
   docker cp $(docker create --rm ruby-plugin-alpine:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/${GEM_NAME}_${TAG}_alpine_linux_amd64.tar.gz
 
   # Build CentOS asset
-  docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-centos:latest -f bonsai/Dockerfile.centos .
+  docker build --build-arg "ASSET_GEM=${GEM_NAME}" --build-arg "GIT_REPO=${GIT_REPO}"  --build-arg "GIT_REF=${GIT_REF}" -t ruby-plugin-centos:latest -f Dockerfile.centos .
   docker cp $(docker create --rm ruby-plugin-centos:latest sleep 0):/${GEM_NAME}.tar.gz ./dist/${GEM_NAME}_${TAG}_centos_linux_amd64.tar.gz
 
   # Generate the sha512sum for all the assets
   files=$( ls dist/*.tar.gz )
   echo $files
   for filename in $files; do
-    if [[ "$GITHUB_RELEASE_TAG" ]]; then
+    if [[ "$TRAVIS_TAG" ]]; then
       echo "upload $filename"
-      bonsai/github-release-upload.sh github_api_token=$GITHUB_TOKEN repo_slug="$GIT_OWNER_REPO" tag="${GITHUB_RELEASE_TAG}" filename="$filename"
+      travis/github-release-upload.sh github_api_token=$GITHUB_TOKEN repo_slug="$TRAVIS_REPO_SLUG" tag="${TRAVIS_TAG}" filename="$filename"
     fi
   done 
   file=$(basename "${files[0]}")
@@ -55,9 +54,9 @@ if [ -d dist ]; then
     echo ""
     cat "${sha512_file}"
     cd ..
-    if [[ "$GITHUB_RELEASE_TAG" ]]; then
+    if [[ "$TRAVIS_TAG" ]]; then
       echo "upload ${sha512_file}"
-      bonsai/github-release-upload.sh github_api_token=$GITHUB_TOKEN repo_slug="$GIT_OWNER_REPO" tag="${GITHUB_RELEASE_TAG}" filename="dist/${sha512_file}"
+      travis/github-release-upload.sh github_api_token=$GITHUB_TOKEN repo_slug="$TRAVIS_REPO_SLUG" tag="${TRAVIS_TAG}" filename="dist/${sha512_file}"
     fi
   fi
 
